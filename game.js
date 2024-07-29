9f4d244e90fd27e9c48548f55f922c40140a1143
Hertrax <4ssume1t@gmail.com>
2024-07-30 00:04:47 +0500

    fixed the problems

--- game.js
+++ game.js
@@ -1,37 +1,113 @@
 
-user = prompt('Welcome! enter j for rock, k for paper, and l for paper.');
+let options = ["rock", "paper", "scissors"];
+/*This is the actual game that will be played*/
 
-function converter(user) {
-    if ( user == "j") {
-        return user = "rock";
+function checkWinner(playerselection, computerselection) {
+
+    if ((playerselection == 'rock' && computerselection == options[2]) ||
+       (playerselection == 'paper' && computerselection == options[0]) ||
+       (playerselection == 'scissors' && computerselection == options[0])) {
+       return 'player';
     }
-    else if (user == "k") {
-        return user = "paper";
+    else if ((playerselection == "scissors" && computerselection == options[0]) ||
+             (playerselection == "rock" && computerselection == options[1]) ||
+             (playerselection == "paper" && computerselection == options[2])) {
+             return 'computer';
+            }
+    else   {
+            return 'draw';
+            }
+    } 
+
+    function getplayerselection() {
+        let validatedInput = false;
+        while(validatedInput == false) {
+            const choice = prompt("Rock Paper or Scissors");
+            if (choice == null) {
+                validatedInput == false;
+                continue;
+            }
+            const choiceInLower = choice.toLowerCase();
+            if (options.includes(choiceInLower)) {
+                validatedInput == true;
+                return choiceInLower;
+            }
+        }
     }
-    else if (user == "l") {
-        return user = "scissors";
+
+function playRound(playerselection, computerselection) {
+    let result = checkWinner(playerselection, computerselection);
+    
+    if (result == 'draw') {
+        return "It's a draw";
+    }
+    else if (result == 'player') {
+        return "player won!" + playerselection + " beats " +  computerselection;
     }
     else {
-        return alert("choose an option to play.");
+        return 'You lose! ' + computerselection + " beats " + playerselection;
     }
 }
 
-options = ["rock", "paper", "scissors"];
+function getCompChoice() {
+    return ((options[(Math.floor(Math.random() * options.length))]));
+}
 
-userscore = 0;
-compscore = 0;
-comp = (options[(Math.floor(Math.random() * options.length))]);
-score = 0;
-message = "";
+function game() {
+    console.log('Welcome!')
+    let userscore = 0;
+    let compscore = 0;
+    for(i = 0;i < 5; i++){
+        const playerselection= getplayerselection();
+        const computerselection = getCompChoice();
+        console.log(playRound(playerselection, computerselection));
+        console.log('------------------')
+        if (checkWinner(playerselection, computerselection)== 'player') {
+            userscore++;
+        }
+        else if(checkWinner(playerselection, computerselection) =='computer') {
+            compscore++;
+        }
+    }
+    console.log('Game Over!');
+    if (userscore > compscore) {
+        console.log('player wins!');
+    }
+    else if (compscore > userscore) {
+        console.log('Computer wins!');
+    }
+    else {
+        console.log('Its a draw!');
+    }
+} 
 
-playgame(user,comp);
-game(user, comp);
 
-function game(user, comp) {
-    playgame(user, comp);
-    playgame(user, comp);
-    playgame(user, comp);
-    playgame(user, comp);
+game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function getCompChoice() {
+    return ((options[(Math.floor(Math.random() * options.length))]));
+}
+ 
+function getUserChoice() {
+    let user = document.getElementById('button').value;
+    let userChoice = user.toLowerCase();
+    if ((userChoice == 'scissors') || (userChoice == 'rock') || (userChoice == 'paper')) {
 } 
 
 function draw(user, comp) {
@@ -39,37 +115,19 @@ function draw(user, comp) {
         compscore += 1;
         message = "Its a draw";
         score = userscore + " : " + compscore;
-        return (alert(message), userscore, compscore, alert(score));
+        return (alert(message), alert(score));
 }
 
 function userpoint(user, comp) {
             userscore += 1;
             score = userscore + " : " + compscore;
             message = "You got a point";
-            return(alert(message), userscore, compscore, alert(score));
+            return(alert(message), alert(score));
 }
 
 function compoint(user, comp ) {
     compscore += 1;
     message = "Computer got a point"
-    return(alert(message),userscore, compscore,  alert(score));
-}
-function playgame(user, comp) {
-    user = prompt ("enter j fot rock, k for paper and l for scissors");
-    converter(user);
-
-    if (user == comp) {
-        draw(user, comp);
-    }
-    else if ((user == "scissors" && comp == options[0]) ||
-             (user == "rock" && comp == options[1]) ||
-             (user == "paper" && comp == options[2])) {
-             compoint(user, comp);
-            }
-    else if ((user == "rock" && comp == options[1]) ||
-             (user == "paper" && comp == options[2]) ||
-             (user == "scissors" && comp == options[0])) {
-             userpoint(user, comp);
-             }
-    else {};
-    }
+    score = userscore + ' : ' + compscore;
+    return(alert(message),  alert(score));
+}*/
