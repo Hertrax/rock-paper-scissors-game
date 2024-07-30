eea481398a1b0b3236bce913486ecaf466fce4c8
Hertrax <4ssume1t@gmail.com>
2024-07-30 19:41:51 +0500

    cleaned up

--- game.js
+++ game.js
@@ -6,7 +6,6 @@ let compscoredisplay = document.getElementById("compscore");
 let userscore = 0;
 let compscore = 0;
 
-/*This is the actual game that will be played*/
 
 function checkWinner(playerselection, computerselection) {
 
@@ -24,22 +23,7 @@ function checkWinner(playerselection, computerselection) {
             return 'draw';
             }
     } 
-
-    /*function getplayerselection() {
-        let validatedInput = false;
-        while(validatedInput == false) {
-            const choice = prompt("Rock Paper or Scissors");
-            if (choice == null) {
-                validatedInput == false;
-                continue;
-            }
-            const choiceInLower = choice.toLowerCase();
-            if (options.includes(choiceInLower)) {
-                validatedInput == true;
-                return choiceInLower;
-            }
-        }
-    }*/
+ 
 
 function playRound(playerselection) {
     let computerselection = getCompChoice();
@@ -61,39 +45,8 @@ function playRound(playerselection) {
     }
 }
 
+
 function getCompChoice() {
     return ((options[(Math.floor(Math.random() * options.length))]));
 };
 
-/*
-function getCompChoice() {
-    return ((options[(Math.floor(Math.random() * options.length))]));
-}
- 
-function getUserChoice() {
-    let user = document.getElementById('button').value;
-    let userChoice = user.toLowerCase();
-    if ((userChoice == 'scissors') || (userChoice == 'rock') || (userChoice == 'paper')) {
-} 
-
-function draw(user, comp) {
-        userscore += 1;
-        compscore += 1;
-        message = "Its a draw";
-        score = userscore + " : " + compscore;
-        return (alert(message), alert(score));
-}
-
-function userpoint(user, comp) {
-            userscore += 1;
-            score = userscore + " : " + compscore;
-            message = "You got a point";
-            return(alert(message), alert(score));
-}
-
-function compoint(user, comp ) {
-    compscore += 1;
-    message = "Computer got a point"
-    score = userscore + ' : ' + compscore;
-    return(alert(message),  alert(score));
-}*/
